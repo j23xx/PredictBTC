d5c047f566fe60ada4b7fffdbd6d548ea7230240
j23xx <j23rdx@gmail.com>
2025-08-28 16:38:38 +0800

    v1.1 build

--- btc-usdt-chart/next.config.ts
+++ btc-usdt-chart/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  trailingSlash: false,
+  skipTrailingSlashRedirect: false,
 };
 
 export default nextConfig;
